6664761111593def938a61dc1a2cf47cf4836243
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-07-15 22:25:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html","utf-8");
+
+const replaceVal = (tempVal,orgVal) =>{
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    return temperature; 
+};
+
+const server = http.createServer((req,res) => {
+    if(req.url =="/"){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=bb63cd3c88637b8aa95b973243af5b76")
+        .on('data', function (chunk) {
+            const objData = JSON.parse(chunk);
+            const arrData = [objData];
+        // console.log(arrData); 
+        const realTimeData = arrData
+        .map((val) => replaceVal(homeFile,val))
+        .join(""); 
+        console.log(realTimeData); 
+        res.write(realTimeData); 
+    })
+    .on('end', function (err) {
+    if (err) return console.log('connection closed due to errors', err);
+    res.end();
+    });
+    }
+});
+
+server.listen(8000,"127.0.0.1");
+
